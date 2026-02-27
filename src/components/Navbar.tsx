2dca481a1cd05bd8703f9018f19a2734b78df99a
rajeshvarsha67-droid <rajeshvarsha67@gmail.com>
2026-02-28 00:51:06 +0530

    Enlarge navbar logo from 48px to 72px

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -24,8 +24,8 @@ export default function Navbar() {
                     <Image
                         src="/images/logo.png"
                         alt="Dyuthi Dance Studio"
-                        width={48}
-                        height={48}
+                        width={72}
+                        height={72}
                         className="object-contain"
                     />
                     <span className="text-xl font-bold text-slate-900 hidden sm:block">
